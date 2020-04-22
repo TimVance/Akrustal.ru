d956e702a7e91cd6446aefef4777d53ef42d73ca
TimVance <2DngxvshA6GUGcQ>
2020-04-22 15:31:32 +0300

    Сделал разные карты для страницы где купить и контакты

--- contacts/index.php
+++ contacts/index.php
@@ -1,2 +1,8 @@
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Контакты");?><?CMax::ShowPageType('page_contacts');?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+$APPLICATION->SetTitle("Контакты");?>
+
+
+<?CMax::ShowPageType('page_contacts');?>
+
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
